b16c5bebea72b30aac06fd666c78a3b57df45d22
Mohd Hussain <samsteve124@gmail.com>
2020-01-19 18:12:13 +0530

    date added #14

--- src/components/DateContainer.js
+++ src/components/DateContainer.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react'
 import ItemContainer from './ItemContainer'
 
-export default class DateContainer extends Component {
+const month = ['January', 'February','March','April','May','June','July','August','September','October','November','December']
+const day = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+
+class DateContainer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            date:new Date()
+        }
+    }
     render() {
+        const {date} = this.state
         return (
             <React.Fragment>
             <div className="itemContainer">
                 <div style={{flex:'80%'}}>
-                    <span id="date">12</span>
+                    <span id="date">{date.getDate()}</span>
                     <div style={{display:'inline-block'}}>
-                        <div>July</div>
-                        <div>2019</div>
+                        <div>{month[date.getMonth()]}</div>
+                        <div>{date.getFullYear()}</div>
                     </div>
                 </div>
                 <div style={{flex:'20%',alignSelf:'center', fontSize:'2rem'}}>
-                    <span>Friday</span>
+                    <span>{day[date.getDay()]}</span>
                     </div>
             </div>
             <ItemContainer/>
@@ -22,3 +32,5 @@ export default class DateContainer extends Component {
         )
     }
 }
+
+export default DateContainer

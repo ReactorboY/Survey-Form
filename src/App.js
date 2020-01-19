5871df5890874e2875523be52b9e500eab057f88
Mohd Hussain <samsteve124@gmail.com>
2020-01-19 15:08:47 +0530

    new name

--- src/App.js
+++ src/App.js
@@ -1,70 +1,20 @@
 import React, { Component } from 'react'
+import TodoContainer from './components/TodoContainer'
 import './App.css'
-
-const DEFAULT_QUERY = 'react'
-const PATH_BASE = 'https://hn.algolia.com/api/v1';
-const PATH_SEARCH = '/search';
-const PARAM_SEARCH = 'query=';
-
-const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      list:null
+      
     }
-    this.onDismiss = this.onDismiss.bind(this)
-    this.setSearchTopStories = this.setSearchTopStories.bind(this)
-  }
-
-  onDismiss(id) {
-    const isNotId = item => item.objectID !== id;
-    const updatedHits = this.state.list.filter(isNotId);
-    this.setState({
-      list: updatedHits
-    });
-  }
-
-  setSearchTopStories = result => this.setState({list:result})
-
-  componentDidMount() {
-    fetch(`${url}`)
-      .then(res =>  res.json())
-      .then(result => this.setSearchTopStories(result.hits))
-      .catch(e => console.log(e))
   }
 
   render() {
-    const {list} = this.state
-    if (!list) return null
     return (
-      <div className="table">
-                {list.map(item => 
-                    <div key={item.objectID} className="table-row">
-                        <span style={{ width: '40%' }}>
-                        <a href={item.url}>{item.title}</a>
-                        </span>
-                        <span style={{ width: '30%' }}>
-                        {item.author}
-                        </span>
-                        <span style={{ width: '10%' }}>
-                        {item.num_comments}
-                        </span>
-                        <span style={{ width: '10%' }}>
-                        {item.points}
-                        </span>
-                        <span style={{ width: '10%' }}>
-                        <button
-                            onClick={() => this.onDismiss(item.objectID)}
-                            className="button-inline"
-                        >
-                            Dismiss
-                        </button>
-                        </span>
-                    </div>    
-                )}
-            </div>
+      <main>
+       <TodoContainer/>
+      </main>  
     )
   }
 }
---
+++ src/components/TodoContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class TodoContainer extends Component {
+    render() {
+        return (
+            <div id="todoContainer">
+                <h1>Hi</h1>
+            </div>
+        )
+    }
+}
+
+export default TodoContainer

db7f801ebb19f83a190fe7dc535db490e7802b74
Mohd Hussain <samsteve124@gmail.com>
2020-01-26 10:43:53 +0530

    signup completed now time for functionality

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -16,55 +16,74 @@ class SignUpForm extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            
+            name:'',
+            email:'',
+            password:''
         }
     }
+
+    onSubmit = e => {
+        e.preventDefault()
+    }
+
+    onChange = e => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
     render() {
+        const {name, email, password} = this.state
         return (
             <div className="scontainer flex-center">
-                <form className="signup-form flex-center">
-                    <div className="form-group" style={{justifySelf:"flex-start"}}>
-                        <p style={{fontSize:"30px"}}>Sign Up</p>
-                    </div>
-                    <div className="form-group">
-                        <div className="icon">
-                            <i className="material-icons">
-                            how_to_reg
-                            </i>
+                <div className="signup-form flex-center">
+                    <form onSubmit={this.onSubmit}>
+                        <div className="form-group" style={{justifySelf:"flex-start"}}>
+                            <p style={{fontSize:"30px"}}>Sign Up</p>
                         </div>
-                        <input type="text" placeholder="Enter Name" className="no-border bottom-border" />
-                    </div>
-                    <div className="form-group">
-                        <div className="icon">
-                            <i className="material-icons">
-                            email
-                            </i>
+                        <div className="form-group">
+                            <div className="icon">
+                                <i className="material-icons">
+                                how_to_reg
+                                </i>
+                            </div>
+                            <input type="text" name="name" onChange={this.onChange} value={name} placeholder="Enter Name" className="no-border bottom-border" />
                         </div>
-                        <input type="email" placeholder="Enter email" className="no-border bottom-border" />
-                    </div>
-                    <div className="form-group">
-                        <div className="icon">
-                            <i className="material-icons">
-                            lock
-                            </i>
+                        <div className="form-group">
+                            <div className="icon">
+                                <i className="material-icons">
+                                email
+                                </i>
+                            </div>
+                            <input type="email" name="email" onChange={this.onChange} value={email} placeholder="Enter email" className="no-border bottom-border" />
                         </div>
-                        <input type="password" placeholder="Enter password" className="no-border bottom-border" />
-                    </div>
-                    <div className="form-group">
-                        <div style={{paddingBottom:"25px"}}>
-                            <input type="checkbox" id="terms" name="terms"
-                            defaultChecked/>
-                            <label htmlFor="terms">I am agree to all terms & conditions</label>
+                        <div className="form-group">
+                            <div className="icon">
+                                <i className="material-icons">
+                                lock
+                                </i>
+                            </div>
+                            <input type="password" name="password" onChange={this.onChange} value={password} placeholder="Enter password" className="no-border bottom-border" />
                         </div>
-                        <button className="submit-button" type="submit">Sign Up</button>
-                    </div>
-                    <div className="form-group">
-                        <button className="googleSignin"></button>
+                        <div className="form-group">
+                            <div style={{paddingBottom:"25px"}}>
+                                <input type="checkbox" id="terms" name="terms"
+                                defaultChecked/>
+                                <label htmlFor="terms">I am agree to all terms & conditions</label>
+                            </div>
+                            <button className="submit-button" type="submit">Sign Up</button>
                     </div>
                 </form>
-                <div className="signup-info flex-center">
+                <div style={{marginTop:"1rem"}}>
+                    <button className="social-button">
+                        <img src="/img/search.svg" alt="google sign in"/>
+                    </button>
+                    <button className="social-button">
+                        <img src="/img/github.svg" alt="Github Sign in" style={{width:"100%",height:"100%"}}/>
+                    </button>
                 </div>
-            </div>     
+            </div>
+            <div className="signup-info flex-center">
+            </div>
+        </div>     
         )
     }
 }

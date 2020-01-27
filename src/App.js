ad8bddb73467c2266dcc6f8f92ae1ea7e8001e69
Mohd Hussain <samsteve124@gmail.com>
2020-01-27 19:01:04 +0530

    session handling complete #26

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React , {Component}from 'react'
 import {BrowserRouter as Router,Route} from 'react-router-dom'
 import * as ROUTES from './components/constants/routes'
+import {withAuthentication} from './components/Session'
 import Landing from './components/Landing'
 import SignUp from './components/SignUp'
 import SignIn from './components/SignIn'
@@ -14,16 +15,17 @@ class App extends Component {
       authUser:null
     }
   }
+
   render() {
     return (
-      <Router>
-        <Route exact path={ROUTES.LANDING} component={Landing}/>
-        <Route path={ROUTES.SIGNUP} component={SignUp}/>
-        <Route path={ROUTES.SIGNIN} component={SignIn}/>
-        <Route path={ROUTES.HOME} component={Home}/>
-    </Router>
+        <Router>
+          <Route exact path={ROUTES.LANDING} component={Landing}/>
+          <Route path={ROUTES.SIGNUP} component={SignUp}/>
+          <Route path={ROUTES.SIGNIN} component={SignIn}/>
+          <Route path={ROUTES.HOME} component={Home}/>
+        </Router>
     )
   }
 }
 
-export default App
+export default withAuthentication(App)
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import {withAuthorization} from '../Session'
 
-const Home = () => {
+const Home = props => {
+    const onClickk = e => {
+        props.firebase.doSignOut()
+    }
     return (
-        <div>
-            Hey
+        <div className="flexy">
+            <div className="scontainer flex-center">
+                <h1>Hi</h1>
+                <button onClick={onClickk}>Signout</button>
+            </div>
         </div>
     )
 }
 
-export default Home
+const condition = authUser => !!authUser
+
+export default withAuthorization(condition)(Home)
